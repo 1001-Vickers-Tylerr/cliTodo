6d3e02a98a21b58898a2bca3e88a42cec701e66e
VickersCode <tyler31vickers@gmail.com>
2025-04-30 16:50:12 -0700

    can now update priority

--- todo-cli-api/api/routes.py
+++ todo-cli-api/api/routes.py
@@ -14,3 +14,13 @@ async def add_todo(item: TodoItem):
         raise HTTPException(status_code=400, detail="Todo with this ID already exists.")
     todos.append(item)
     return item
+
+@router.put("/todos/{id}")
+async def update_todo(id: int, item: TodoItem):
+    if item.id != id:
+        raise HTTPException(status_code=400, detail="ID must match.")
+    for i, todo in enumerate(todos):
+        if todo.id == id:
+            todos[i] = item
+            return item
+    raise HTTPException(status_code=404, detail="Todo not found")
